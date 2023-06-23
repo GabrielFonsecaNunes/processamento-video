78934f10d2e01c8fcc63815d9825d772120851f1
GabrielFonsecaNunes <fonseca.nunes1996@gmail.com>
2023-06-23 04:00:50 -0300

    Update Files

--- semana2/Atividade1/src/script.js
+++
@@ -1,7 +0,0 @@
-function copyCode(containerId) {
-    var container = document.getElementById(containerId);
-    var textarea = container.querySelector('textarea');
-    textarea.select();
-    document.execCommand('copy');
-    alert('Código copiado para a área de transferência!');
-}
